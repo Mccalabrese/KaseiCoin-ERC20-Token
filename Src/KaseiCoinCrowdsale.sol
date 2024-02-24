631b0ddad183dd5d32273e70e9e4c87aff5af2e1
Mccalabrese <138624063+Mccalabrese@users.noreply.github.com>
2024-02-24 11:51:56 -0600

    Update KaseiCoinCrowdsale.sol

--- Src/KaseiCoinCrowdsale.sol
+++ Src/KaseiCoinCrowdsale.sol
@@ -3,19 +3,30 @@ pragma solidity ^0.5.0;
 import "./KaseiCoin.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/CappedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/TimedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
 
 
 // Have the KaseiCoinCrowdsale contract inherit the following OpenZeppelin:
 // * Crowdsale
 // * MintedCrowdsale
-contract KaseiCoinCrowdsale is Crowdsale, MintedCrowdsale { // UPDATE THE CONTRACT SIGNATURE TO ADD INHERITANCE
+contract KaseiCoinCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCrowdsale, RefundablePostDeliveryCrowdsale { // UPDATE THE CONTRACT SIGNATURE TO ADD INHERITANCE
     
     // Provide parameters for all of the features of your crowdsale, such as the `rate`, `wallet` for fundraising, and `token`.
     constructor(
         uint256 rate,
         address payable wallet,
-        Kaseicoin token
-    ) Crowdsale(rate, wallet, token) public {
+        Kaseicoin token,
+        uint256 goal,
+        uint256 open,
+        uint256 close
+    ) public 
+    Crowdsale(rate, wallet, token)
+    CappedCrowdsale(goal)
+    TimedCrowdsale(open, close)
+    RefundableCrowdsale(goal)
+     {
         // constructor can stay empty
     }
 }
@@ -32,7 +43,8 @@ contract KaseiCoinCrowdsaleDeployer {
     constructor(
        string memory name,
        string memory symbol,
-       address payable wallet
+       address payable wallet,
+       uint256 goal
     ) public {
         // Create a new instance of the KaseiCoin contract.
         Kaseicoin token = new Kaseicoin(name, symbol, 0);
@@ -41,7 +53,7 @@ contract KaseiCoinCrowdsaleDeployer {
         kasei_token_address = address(token);
 
         // Create a new instance of the `KaseiCoinCrowdsale` contract
-        KaseiCoinCrowdsale kai_crowdsale = new KaseiCoinCrowdsale(1, wallet, token);
+        KaseiCoinCrowdsale kai_crowdsale = new KaseiCoinCrowdsale(1, wallet, token, goal, now, now + 24 weeks);
             
         // Aassign the `KaseiCoincrowdsale` contract’s address to the `kasei_crowdsale_address` variable.
         kasei_crowdsale_address = address(kai_crowdsale);
